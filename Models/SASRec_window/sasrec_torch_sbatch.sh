0ce1e648b65eca9ecaebb4812a4ee4bb1fcabfc8
kyriakossk2000 <kyriakoskyriakou2000@gmail.com>
2023-06-13 09:41:59 +0100

    run experiements with windows

--- Models/SASRec_window/sasrec_torch_sbatch.sh
+++ Models/SASRec_window/sasrec_torch_sbatch.sh
@@ -37,5 +37,5 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mscproject
 
 #python main.py --device=cuda --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2
 #python main.py --device=cuda --dataset=Video --train_dir=default
-python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=false --window_eval=true --device=cuda
-#python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=true --window_eval=true --device=cuda
+#python main.py --dataset=ml-1m --train_dir=default --maxlen=201 --dropout_rate=0.2 --window_split=false --window_eval=true --device=cuda
+python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=true --window_eval=true --device=cuda
