e1f76104da6158043059b28c2764eba98e1f906d
kyriakossk2000 <kyriakoskyriakou2000@gmail.com>
2023-06-17 18:33:04 +0100

    experiements

--- Models/SASRec_window/sasrec_torch_sbatch.sh
+++ Models/SASRec_window/sasrec_torch_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Teach-Standard
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:titan-x:1
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
@@ -37,6 +37,7 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mscproject
 
 #python main.py --device=cuda --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2
 #python main.py --device=cuda --dataset=Video --train_dir=default
-#python main.py --dataset=ml-1m --train_dir=default --maxlen=201 --dropout_rate=0.2 --window_split=false --window_eval=true --device=cuda
-#python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=true --window_eval=true --device=cuda
-python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=false --window_eval=true --inference_only=true --state_dict_path=SASRec_dict_baselien_no_split.pth  --device=cuda
+python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=false --window_eval=true --device=cuda
+#python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=true --window_eval=true --device=cuda --num_epochs=100
+#python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=false --window_eval=true --inference_only=true --state_dict_path=SASRec_dict_baselien_no_split.pth  --device=cuda
+#python main.py --dataset=ml-1m --train_dir=default --maxlen=200 --dropout_rate=0.2 --window_split=true --window_eval=true --all_action=true --device=cuda
