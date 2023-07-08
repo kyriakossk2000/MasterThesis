masking fix?

Models/SASRec_window/main.py
@@ -323,9 +323,12 @@ if __name__ == '__main__':
                     loss = loss.mean()  # avg over window size 
                     if args.masking:
                         mask_indices = np.where(mask == 1)
-                        mask_logits, _ = model(u, masked_seq, seq, neg)
-                        pos_labels = torch.ones(mask_logits.shape, device=args.device)
-                        mask_loss = criterion(mask_logits[mask_indices], pos_labels[mask_indices])
+                        mask_pos_logits, mask_neg_logits = model(u, masked_seq, seq, neg[:,:,0])
+                        mask_logits = torch.cat((mask_pos_logits[mask_indices], mask_neg_logits[mask_indices]), dim=0)
+                        mask_pos_labels = torch.ones(mask_pos_logits.shape, device=args.device)
+                        mask_neg_labels = torch.zeros(mask_neg_logits.shape, device=args.device)
+                        mask_labels = torch.cat((mask_pos_labels[mask_indices], mask_neg_labels[mask_indices]), dim=0)
+                        mask_loss = criterion(mask_logits, mask_labels)
                         loss += mask_loss
                 else:
                     if args.model_training == 'all_action':

Models/SASRec_window/model.py
@@ -118,10 +118,10 @@ class SASRec(torch.nn.Module):
                         neg_logits_list.append(neg_logits)
                 elif pos_seqs.ndim == 2:
                     pos_samples_embeddings = self.item_emb(torch.LongTensor(pos_seqs).to(self.dev))
-
+                    neg_samples_embeddings = self.item_emb(torch.LongTensor(neg_seqs).to(self.dev))
                     pos_logits = (log_feats * pos_samples_embeddings).sum(dim=-1)
-                    
-                    return pos_logits, None
+                    neg_logits = (log_feats * neg_samples_embeddings).sum(dim=-1)
+                    return pos_logits, neg_logits
 
                 return pos_logits_list, neg_logits_list
             