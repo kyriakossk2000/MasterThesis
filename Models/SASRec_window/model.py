aeb464defb474e3fc9486c410b6752bec3b9ad02
kyriakossk2000 <kyriakoskyriakou2000@gmail.com>
2023-07-10 00:37:31 +0100

    fix?

--- Models/SASRec_window/model.py
+++ Models/SASRec_window/model.py
@@ -106,7 +106,7 @@ class SASRec(torch.nn.Module):
                 neg_logits_list = []
 
                 if self.strategy in ['autoregressive', 'teacher_forcing']:
-                    seqs = torch.LongTensor(log_seqs)  # to a PyTorch tensor
+                    seqs = torch.from_numpy(log_seqs).long().to(self.dev)  # o a PyTorch tensor
 
                     for i in range(self.window_size):
 
