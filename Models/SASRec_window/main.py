fix Future rolling + added uniform sampled softmax

Models/SASRec_window/main.py
@@ -38,6 +38,7 @@ parser.add_argument('--loss_type', default='bce', type=str)                 # lo
 parser.add_argument('--strategy', default='default', type=str)              # training strategy
 parser.add_argument('--masking', default=False, type=str2bool)              # masking or not
 parser.add_argument('--mask_prob', default=0.15, type=float)                # mask probability
+parser.add_argument('--uniform_ss', default=False, type=str2bool)           # use uniform neg sampled softmax loss? 
 
 args = parser.parse_args()
 if not os.path.isdir(args.dataset + '_' + args.train_dir):
@@ -53,6 +54,8 @@ if __name__ == '__main__':
         pass
     else:
         print("Data partition: ", args.data_partition)
+    if args.masking:
+        print("Masking with perc: ", args.mask_prob)
     # load dataset
     if args.model_training == 'all_action':
         dataset = data_partition_window_all_action(args.dataset, window_size=args.window_size, target_seq_percentage=0.9)
@@ -69,7 +72,7 @@ if __name__ == '__main__':
             if count >= 3:  # Change this to print more or fewer sequences
                 break
         
-        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, loss_type=args.loss_type)
+        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, args=args)
 
     elif args.model_training == 'dense_all_action':
         dataset = data_partition_window_all_action(args.dataset, window_size=args.window_size, target_seq_percentage=0.9)
@@ -85,7 +88,7 @@ if __name__ == '__main__':
             count += 1
             if count >= 3:  # Change this to print more or fewer sequences
                 break
-        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, loss_type=args.loss_type)
+        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, args=args)
     elif args.model_training == 'super_dense_all_action':
         dataset = data_partition_window_all_action(args.dataset, window_size=args.window_size, target_seq_percentage=0.9)
         [user_input_seq, user_target_seq, user_train, user_valid, user_test, usernum, itemnum] = dataset
@@ -100,8 +103,7 @@ if __name__ == '__main__':
             count += 1
             if count >= 3:  # Change this to print more or fewer sequences
                 break
-        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, loss_type=args.loss_type)
-    
+        sampler = WarpSamplerAll(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, model_training=args.model_training, window_size=args.window_size, args=args)    
     elif args.model_training == 'future_rolling':
         dataset = data_partition_window_rolling(args.dataset, window_size=args.window_size, target_seq_percentage=0.9)
         [user_input_seq, user_target_seq, user_train, user_valid, user_test, usernum, itemnum] = dataset
@@ -116,7 +118,7 @@ if __name__ == '__main__':
             count += 1
             if count >= 3:  # Change this to print more or fewer sequences
                 break
-            sampler = WarpSamplerRolling(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, window_size=args.window_size, loss_type=args.loss_type)
+        sampler = WarpSamplerRolling(user_input_seq, user_target_seq, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3, window_size=args.window_size, args=args)
         
     else:  # SASRec next item
         if args.data_partition == 'independent':
@@ -230,7 +232,7 @@ if __name__ == '__main__':
     # ce_criterion = torch.nn.CrossEntropyLoss()
     # https://github.com/NVIDIA/pix2pixHD/issues/9 how could an old bug appear again...
     if args.loss_type == 'sampled_softmax':
-        if args.model_training == 'all_action' or args.model_training == 'future_rolling':
+        if not args.uniform_ss:
             criterion = SampledSoftmaxLossOver()
         else:
             criterion = SampledSoftmaxLoss()
@@ -238,8 +240,13 @@ if __name__ == '__main__':
         criterion = torch.nn.CrossEntropyLoss()
     else:
         criterion = torch.nn.BCEWithLogitsLoss() 
-    print('Loss type: ', args.loss_type)
-    
+    if args.loss_type == 'sampled_softmax' and args.uniform_ss:
+        print('Loss type: Uniform Sampled Softmax')
+    elif args.loss_type == 'sampled_softmax' and not args.uniform_ss:
+        print('Loss type: Sampled Softmax with LogQ')
+    else:
+        print('Loss type: ', args.loss_type)
+
     if args.optimizer == 'sam':
         base_optimizer = torch.optim.Adam
         sam_optimizer = SAM(model.parameters(), base_optimizer, lr=args.lr)
@@ -261,7 +268,7 @@ if __name__ == '__main__':
                 mask = np.random.choice([0, 1], size=(seq.shape[0], seq.shape[1]), p=[1-mask_prob, mask_prob])
                 masked_seq = np.where(mask==1, 0, seq)
             
-            if (args.model_training == 'future_rolling' or args.model_training == 'all_action') and args.loss_type == 'sampled_softmax':
+            if not args.uniform_ss and args.loss_type == 'sampled_softmax':
                 pos_logits, neg_logits, neg_logQ = model(u, seq, pos, neg)
             else:
                 pos_logits, neg_logits = model(u, seq, pos, neg)
@@ -297,14 +304,17 @@ if __name__ == '__main__':
             else:
                 adam_optimizer.zero_grad()
                 if args.loss_type == 'sampled_softmax':
-                    if args.model_training == 'all_action' or args.model_training == 'future_rolling':
+                    if (args.model_training == 'all_action' or args.model_training == 'future_rolling') and not args.uniform_ss:
                         loss = criterion(pos_logits, neg_logits, neg_logQ)
                     else:
-                        loss = criterion(pos_logits, neg_logits) # compute the loss using SampledSoftmaxLoss
-                        if args.masking:
-                            mask_pos_logits, mask_neg_logits = model(u, masked_seq, seq, neg)
-                            mask_loss = criterion(mask_pos_logits, mask_neg_logits)
-                            loss += mask_loss
+                        loss = 0
+                        for i in range(args.window_size):
+                            loss += criterion(pos_logits[i], neg_logits[i])
+                        loss = loss.mean()  # avg over window size 
+                    if args.masking:
+                        mask_pos_logits, mask_neg_logits = model(u, masked_seq, seq, neg)
+                        mask_loss = criterion(mask_pos_logits, mask_neg_logits)
+                        loss += mask_loss
                 elif args.loss_type == 'ce_over':
                     loss = 0
                     for i in range(args.window_size):

Models/SASRec_window/model.py
@@ -33,6 +33,7 @@ class SASRec(torch.nn.Module):
         self.strategy=args.strategy
         self.window_size=args.window_size
         self.loss_type=args.loss_type
+        self.uniform_ss = args.uniform_ss
 
         # https://stackoverflow.com/questions/42704283/adding-l1-l2-regularization-in-pytorch
         self.item_emb = torch.nn.Embedding(self.item_num+1, args.hidden_units, padding_idx=0)
@@ -187,31 +188,37 @@ class SASRec(torch.nn.Module):
                             neg_embs = self.item_emb(torch.LongTensor(neg_seqs[:, :, i]).to(self.dev))
                             neg_logits = (log_feats * neg_embs).sum(dim=-1)
                             neg_logits_list.append(neg_logits)
+                            if not self.uniform_ss:
+                                neg_logQ = torch.zeros(neg_seqs[:, :, i].shape).to(self.dev)
+                                for k in range(neg_seqs.shape[0]):
+                                    unique_negs, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, i][k]), return_counts=True)  # times neg sample appears in batch
+                                    probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples
+                                    neg_logQ[k] = torch.log(probs).sum(dim=0)
 
-                            neg_logQ = torch.zeros(neg_seqs[:, :, i].shape).to(self.dev)
-                            for k in range(neg_seqs.shape[0]):
-                                unique_negs, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, i][k]), return_counts=True)  # times neg sample appears in batch
-                                probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples
-                                neg_logQ[k] = torch.log(probs).sum(dim=0)
-
-                            neg_logQ_list.append(neg_logQ)
-
-                    return pos_logits_list, neg_logits_list, neg_logQ_list
+                                neg_logQ_list.append(neg_logQ)
+                    if not self.uniform_ss:
+                        return pos_logits_list, neg_logits_list, neg_logQ_list
+                    else:
+                        return pos_logits_list, neg_logits_list
                 else:
                     for i in range(self.window_size):
                         pos_embs = self.item_emb(torch.LongTensor(pos_seqs[:, :, i]).to(self.dev))
                         pos_logits_list.append((log_feats * pos_embs).sum(dim=-1))
                     for j in range(neg_seqs.shape[2]):
                         neg_embs = self.item_emb(torch.LongTensor(neg_seqs[:, :, j]).to(self.dev))
-                        neg_logQ = torch.zeros(neg_seqs[:, :, j].shape).to(self.dev)
-                        for k in range(neg_seqs.shape[0]):
-                            unique_negs, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, j][k]), return_counts=True)  # times neg sample appers in batch
-                            probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples            
-                            neg_logQ[k] = torch.log(probs).sum(dim=0)
-                        neg_logQ_list.append(neg_logQ)
+                        if not self.uniform_ss:
+                            neg_logQ = torch.zeros(neg_seqs[:, :, j].shape).to(self.dev)
+                            for k in range(neg_seqs.shape[0]):
+                                unique_negs, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, j][k]), return_counts=True)  # times neg sample appers in batch
+                                probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples            
+                                neg_logQ[k] = torch.log(probs).sum(dim=0)
+                            neg_logQ_list.append(neg_logQ)
                         neg_logits_list.append((log_feats * neg_embs).sum(dim=-1))
 
-                    return pos_logits_list, neg_logits_list, neg_logQ_list
+                    if not self.uniform_ss:
+                        return pos_logits_list, neg_logits_list, neg_logQ_list
+                    else:
+                        return pos_logits_list, neg_logits_list
             else:
                 pos_samples_embeddings = self.item_emb(torch.LongTensor(pos_seqs).to(self.dev))[:,-1,:]
                 neg_samples_embeddings = self.item_emb(torch.LongTensor(neg_seqs).to(self.dev))[:,-1,:]
@@ -247,7 +254,7 @@ class SASRec(torch.nn.Module):
 
             if self.strategy in ['autoregressive', 'teacher_forcing']:
                 if self.loss_type == 'ce_over':
-                    seqs = torch.LongTensor(log_seqs).to(self.dev)  # o a PyTorch tensor
+                    seqs = torch.LongTensor(log_seqs).to(self.dev)  # to a PyTorch tensor
 
                     for i in range(self.window_size):
 
@@ -271,6 +278,41 @@ class SASRec(torch.nn.Module):
                         seqs = torch.cat([seqs, predicted_action], dim=1)
 
                     return pos_logits_list, neg_logits_list
+                elif self.loss_type == 'sampled_softmax':
+                    seqs = torch.LongTensor(log_seqs).to(self.dev)  # to a PyTorch tensor
+                    
+                    for i in range(max_window_size):
+                        log_feats = self.log2feats(seqs)
+
+                        if i < pos_size:
+                            pos_embs = self.item_emb(torch.LongTensor(pos_seqs[:, :, i]).to(self.dev))
+                            pos_logits = (log_feats * pos_embs).sum(dim=-1)
+                            pos_logits_list.append(pos_logits)
+
+                            if self.strategy == 'autoregressive':
+                                predicted_action = pos_logits.argmax(dim=-1).unsqueeze(1)
+                            elif self.strategy == 'teacher_forcing':
+                                predicted_action = torch.LongTensor(pos_seqs[:, :, i]).to(self.dev)[:,-1].unsqueeze(1)
+
+                            seqs = seqs[:, 1:]  # remove the first element to maintain the embedding size
+                            seqs = torch.cat([seqs, predicted_action], dim=1)
+
+                        if i < neg_size:
+                            neg_embs = self.item_emb(torch.LongTensor(neg_seqs[:, :, i]).to(self.dev))
+                            neg_logits = (log_feats * neg_embs).sum(dim=-1)
+                            neg_logits_list.append(neg_logits)
+                            if not self.uniform_ss:
+                                neg_logQ = torch.zeros(neg_seqs[:, :, i].shape).to(self.dev)
+                                for k in range(neg_seqs.shape[0]):
+                                    unique_negs, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, i][k]), return_counts=True)  # times neg sample appears in batch
+                                    probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples
+                                    neg_logQ[k] = torch.log(probs).sum(dim=0)
+
+                                neg_logQ_list.append(neg_logQ)
+                    if not self.uniform_ss:
+                        return pos_logits_list, neg_logits_list, neg_logQ_list
+                    else:
+                        return pos_logits_list, neg_logits_list
             else:
                 for i in range(self.window_size):
                     pos_embs = self.item_emb(torch.LongTensor(pos_seqs[:, :, i]).to(self.dev))
@@ -285,17 +327,20 @@ class SASRec(torch.nn.Module):
                 elif self.loss_type == 'sampled_softmax':
                     for j in range(neg_seqs.shape[2]):
                         neg_embs = self.item_emb(torch.LongTensor(neg_seqs[:, :, j]).to(self.dev))
-                        neg_logQ = torch.zeros(neg_seqs.shape[0], neg_seqs.shape[1]).to(self.dev)
+                        if not self.uniform_ss:
+                            neg_logQ = torch.zeros(neg_seqs.shape[0], neg_seqs.shape[1]).to(self.dev)
+                            for k in range(neg_seqs.shape[0]):
+                                _, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, j][k]), return_counts=True)  # times neg sample appers in batch
+                                probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples 
+                                neg_logQ[k, :counts.size(0)] = torch.log(probs)
 
-                        for k in range(neg_seqs.shape[0]):
-                            _, counts = torch.unique(torch.LongTensor(neg_seqs[:, :, j][k]), return_counts=True)  # times neg sample appers in batch
-                            probs = counts.float() / neg_seqs.shape[0]  # probs of neg samples 
-                            neg_logQ[k, :counts.size(0)] = torch.log(probs)
-
-                        neg_logQ_list.append(neg_logQ)
+                            neg_logQ_list.append(neg_logQ)
                         neg_logits_list.append((log_feats * neg_embs).sum(dim=-1))
                     
-                    return pos_logits_list, neg_logits_list, neg_logQ_list
+                    if not self.uniform_ss:
+                        return pos_logits_list, neg_logits_list, neg_logQ_list
+                    else:
+                        return pos_logits_list, neg_logits_list
                 else:
                     for j in range(neg_seqs.shape[2]):
                         neg_embs = self.item_emb(torch.LongTensor(neg_seqs[:, :, i]).to(self.dev))

Models/SASRec_window/sampled_softmax.py
@@ -7,15 +7,14 @@ class SampledSoftmaxLoss(torch.nn.Module):
         self.temperature = temperature
 
     def forward(self, pos_logits, neg_logits):
-        # Number of negative samples
+
         num_neg_samples = neg_logits.size(1)
         
-        # Compute s(u_i, p_i) and s(u_i, n_j)
+        # compute s(u_i, p_i) and s(u_i, n_j)
         s_pos = pos_logits / self.temperature
         s_neg = neg_logits / self.temperature
 
-        # Compute logQ correction terms
-        # Note: For simplicity assuming uniform distribution and using a constant logQ.
+        # assuming uniform distribution and using a constant logQ
         log_q_pos = torch.log(torch.tensor(1.0 / num_neg_samples)).to(pos_logits.device)
         log_q_neg = torch.log(torch.tensor(1.0 / num_neg_samples)).to(neg_logits.device)
         
@@ -23,10 +22,9 @@ class SampledSoftmaxLoss(torch.nn.Module):
         
         denominator = numerator + torch.sum(torch.exp(s_neg - log_q_neg), dim=1, keepdim=True)
         
-        # Compute the sampled softmax loss for each example in the batch
+        # get sampled softmax loss for each example in the batch
         loss_per_example = -torch.log(numerator / denominator)
         
-        # Compute the mean loss over the batch
         loss = torch.mean(loss_per_example)
         
         return loss

Models/SASRec_window/utils.py
@@ -23,6 +23,11 @@ def random_neq_all(l, r, s, count):
     np.random.shuffle(possible_numbers)
     return possible_numbers[:count]
 
+def random_neq_all_uniform(l, r, s, count):
+    possible_numbers = list(set(range(l, r)) - set(s))
+    np.random.shuffle(possible_numbers)
+    return np.random.choice(possible_numbers, count, replace=True)
+
 def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
     def sample():
 
@@ -80,7 +85,7 @@ class WarpSampler(object):
             p.terminate()
             p.join()
 
-def sample_function_all(user_train_seq, train_target_seq, usernum, itemnum, batch_size, maxlen, result_queue, SEED, model_training, window_size, loss_type):
+def sample_function_all(user_train_seq, train_target_seq, usernum, itemnum, batch_size, maxlen, result_queue, SEED, model_training, window_size, args):
     def sample():
         neg_samples = window_size
         user = np.random.randint(1, usernum + 1)
@@ -101,8 +106,11 @@ def sample_function_all(user_train_seq, train_target_seq, usernum, itemnum, batc
                 seq[idx] = i 
                 if idx == maxlen - 1:
                     pos[idx] = train_target_sampled
-                    for j in range(neg_samples):
-                        neg[idx,j] = random_neq(1, itemnum + 1, ts)
+                    if args.uniform_ss:
+                        neg[idx] = random_neq_all_uniform(1, itemnum + 1, ts, neg_samples)
+                    else:
+                        for j in range(neg_samples):
+                            neg[idx,j] = random_neq(1, itemnum + 1, ts)
                 idx -= 1
                 if idx == -1: break
 
@@ -140,7 +148,7 @@ def sample_function_all(user_train_seq, train_target_seq, usernum, itemnum, batc
 
 # All action and Dense all action sampler based on Pinnerformer 
 class WarpSamplerAll(object):
-    def __init__(self, user_input_seq, user_target_seq, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1, model_training='all_action', window_size=7, loss_type='bce'):
+    def __init__(self, user_input_seq, user_target_seq, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1, model_training='all_action', window_size=7, args=None):
         self.result_queue = Queue(maxsize=n_workers * 10)
         self.processors = []
         for i in range(n_workers):
@@ -155,7 +163,7 @@ class WarpSamplerAll(object):
                                                         np.random.randint(2e9),
                                                         model_training,
                                                         window_size,
-                                                        loss_type
+                                                        args
                                                         )))
             self.processors[-1].daemon = True
             self.processors[-1].start()
@@ -168,7 +176,7 @@ class WarpSamplerAll(object):
             p.terminate()
             p.join()
 
-def sample_function_rolling(user_input_seq, user_target_seq, usernum, itemnum, batch_size, maxlen, result_queue, SEED, window_size=7, loss_type='bce'):
+def sample_function_rolling(user_input_seq, user_target_seq, usernum, itemnum, batch_size, maxlen, result_queue, SEED, window_size=7, args=None):
     def sample():
         user = np.random.randint(1, usernum + 1)
         while len(user_input_seq[user]) <= 1:
@@ -184,8 +192,11 @@ def sample_function_rolling(user_input_seq, user_target_seq, usernum, itemnum, b
         for i, input_item in enumerate(reversed(user_input_seq[user])):
             seq[idx] = input_item
             pos[idx] = user_target_seq[user][i]
-            for j in range(neg_samples):
-                neg[idx,j] = random_neq(1, itemnum + 1, ts)
+            if args.uniform_ss:
+                neg[idx] = random_neq_all_uniform(1, itemnum + 1, ts, neg_samples)
+            else:
+                for j in range(neg_samples):
+                    neg[idx,j] = random_neq(1, itemnum + 1, ts)
             idx -= 1
             if idx == -1: break
         
@@ -201,7 +212,7 @@ def sample_function_rolling(user_input_seq, user_target_seq, usernum, itemnum, b
 
 
 class WarpSamplerRolling(object):
-    def __init__(self, user_input_seq, user_target_seq, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1, window_size=7, loss_type='bce'):
+    def __init__(self, user_input_seq, user_target_seq, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1, window_size=7, args=None):
         self.result_queue = Queue(maxsize=n_workers * 10)
         self.processors = []
         for i in range(n_workers):
@@ -215,7 +226,7 @@ class WarpSamplerRolling(object):
                                                       self.result_queue,
                                                       np.random.randint(2e9),
                                                       window_size,
-                                                      loss_type
+                                                      args
                                                       )))
             self.processors[-1].daemon = True
             self.processors[-1].start()